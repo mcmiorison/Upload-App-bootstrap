945b61ecbe17fd49ffee95cd6c4e4085b6fe3ec0
Michele Morison <mmorison@umail.iu.edu>
2016-08-12 20:48:25 -0400

    Added information to login page and Create Account form
    css will be added after group feedback

---
+++ scripts/ozu.js
@@ -0,0 +1,33 @@
+function startRead(evt){
+	var file = document.getElementById('file').files[0];
+	if(file){
+		console.log("Name" + file.name );
+		}
+}
+
+//Function to check username availabiltity
+
+$(document).ready(function () {
+
+    //the min chars for username  
+    var min_chars = 10;
+
+    //result texts  
+    var characters_error = 'Minimum amount of chars is 10';
+    var checking_html = 'Checking...';
+
+    //when button is clicked  
+    $('#check_username_availability').click(function () {
+        //run the character number check  
+        if ($('#username').val().length < min_chars) {
+            //if it's bellow the minimum show characters_error text '  
+            $('#username_availability_result').html(characters_error);
+        } else {
+            //else show the cheking_text and run the function to check  
+            $('#username_availability_result').html(checking_html);
+            check_availability();
+        }
+    });
+
+});
+
